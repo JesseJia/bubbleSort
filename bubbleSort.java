b4da6da35b4a00554a723bd2879abf45653326f6
JesseJia <jessejia1989@gmail.com>
2014-10-12 16:13:19 +1100

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -5,9 +5,9 @@ import java.lang.*;
 import java.io.*;
  
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class bubbleSort
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args)
 	{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
